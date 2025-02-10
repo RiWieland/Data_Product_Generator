dddf0d1a9d1cf9a91295a168cce722b024e07a08
RiWieland <Rich.Wieland73@gmail.com>
2025-02-10 14:10:02 +0100

    added error handling for create_source_dir

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,11 +2,14 @@ import os
 import logging
 from config.config_simulation import PATH_SOURCE
 
-def create_source_dir(path: str):
+def create_source_dir(path_list: list):
     """
     creates the dir for the source for the simulation
     """
-    os.mkdir(path)
-    logging.info(f"Directory '{path}' created successfully.")
-
-
+    for path in path_list:
+        try:
+            os.mkdir(path)
+            logging.info(f"Directory '{path}' created successfully.")
+        except FileExistsError:
+            logging.info(f"Directory '{path}' already exists.")
+            continue
