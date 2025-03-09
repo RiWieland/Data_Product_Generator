bcfdc394c1ef1998d7785b1956389a21d54d0ab1
RiWieland <Rich.Wieland73@gmail.com>
2025-03-09 12:40:26 +0100

    added local workers to spark session

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -21,8 +21,9 @@ def get_spark() -> SparkSession:
     create or get a spark session
     """
     builder = SparkSession.builder.appName("MyApp") \
+        .master("local[4]") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
     return spark
