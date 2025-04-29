f9582e6007f470ba571c413606f9e5814204a7a5
RiWieland <Rich.Wieland73@gmail.com>
2025-04-29 13:31:28 +0200

    fix path

---
+++ apps/data_generator/simulation/data_product_one/tables/abstract_table.py
@@ -0,0 +1,10 @@
+"""
+Holder of the abstract table
+"""
+from abc import ABC
+
+
+class DataTable(ABC):
+    """
+    Abstract class for Data Tables
+    """
--- apps/data_generator/simulation/data_product_one/tables/bronze_customer.py
+++ apps/data_generator/simulation/data_product_one/tables/bronze_customer.py
@@ -1,4 +1,4 @@
-from apps.data_generator.simulation.data_product_one.tables.abstract_table import DataTable
+from simulation.data_product_one.tables.abstract_table import DataTable
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType
 
 
