14bfbd7b8b9236c1c9ccc5dfc6fe66b3faffe0ba
RiWieland <Rich.Wieland73@gmail.com>
2025-06-26 10:59:18 +0200

    added psydo test for making sure pytest run with poetry

---
+++ apps/tests/test.py
@@ -0,0 +1,9 @@
+
+from simulation.DP_Sales.bronze.bronze_customer import BronzeCustomer
+
+
+def test_placeholder():
+    """
+    placeholder to make sure poetry could execute tests
+    """
+    assert 1==2
