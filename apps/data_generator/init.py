b86f7954a4f479d62fe55b12b8cc2b890415d291
RiWieland <Rich.Wieland73@gmail.com>
2025-06-27 11:55:38 +0200

    delete file

--- apps/data_generator/init.py
+++
@@ -1,6 +0,0 @@
-
-from utils.utils import create_source_dir
-from config.config_simulation import PATH_SIMULATION, PATH_SOURCE, PATH_TARGET
-
-simultation_dirs = [PATH_SIMULATION, PATH_SOURCE, PATH_TARGET]
-create_source_dir(simultation_dirs)
