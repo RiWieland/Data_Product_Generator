938b487e11a7be0a91299d76c3a9c07989147be1
RiWieland <Rich.Wieland73@gmail.com>
2025-07-02 18:03:59 +0200

    added main for single execution

--- apps/data_generator/main.py
+++ apps/data_generator/main.py
@@ -1,6 +1,6 @@
 
 import os
-from utils.utils import get_spark, create_landing_path_data_sink
+from utils.utils import get_spark
 from config.config_simulation import PATH_DOCKER_SINK, PSYDO_SCHEMA
 from simulation.DP_Sales.gold.gold_fact_revenue import GoldFactRevenue
 from simulation.DP_Sales.bronze.bronze_customer import BronzeCustomer
@@ -8,48 +8,42 @@ from simulation.DP_Sales.bronze.bronze_order import BronzeOrder
 from simulation.DP_Sales.bronze.bronze_product import BronzeProduct
 from data.dataframe_generator import DataframeGenerator
 from data.data_writer import DataWriter
-
+from simulation.DP_Sales.loads.gold_fact_revenue_load import load_gold_fact_revenue
 
 from pyspark.sql.types import StructField, StructField, IntegerType, DoubleType, StringType
 import logging
 import time
 
-start = time.time()
 
-spark = get_spark()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
 
+    start = time.time()
+    spark = get_spark()
 
-# parameter
+    list_tables = [BronzeCustomer, BronzeOrder, BronzeProduct, GoldFactRevenue]
 
+    for i, class_ in enumerate(list_tables):
 
+        table_class_construct = class_()
 
-# 1. All required tables are initialized
-bronze_customer = BronzeCustomer()
-bronze_order = BronzeOrder()
-bronze_product = BronzeProduct()
-gold_fact_revenue = GoldFactRevenue()
+        # Bronze tables are created with generated data
+        if table_class_construct.layer== "bronze":
+        
+            df_data = DataframeGenerator(100000000, table_class_construct.schema).create_df(PSYDO_SCHEMA)
+            DataWriter(df_data, table_class_construct.path, spark).write_df_to_delta()
 
-path_bronze_customer = str(PATH_DOCKER_SINK) +str(create_landing_path_data_sink(bronze_order))
+        else:
+            logging.info("Non-Bronze table called")
+            df_gold = load_gold_fact_revenue()
 
-# 2. The tables are loaded in accordance to the specification
+            DataWriter(df_gold, table_class_construct.path, spark).write_df_to_delta()
 
-df_bronze_customer = DataframeGenerator(100000000, bronze_customer.schema).create_df(PSYDO_SCHEMA)
-df_bronze_order = DataframeGenerator(100000000, bronze_order.schema).create_df(PSYDO_SCHEMA)
-df_bronze_product= DataframeGenerator(100000000, bronze_product.schema).create_df(PSYDO_SCHEMA)
-df_gold_fact = DataframeGenerator(100000000, gold_fact_revenue.schema).create_df(PSYDO_SCHEMA)
-
-path_bronze_customer = str(PATH_DOCKER_SINK) +str(create_landing_path_data_sink(bronze_customer))
-path_bronze_order = str(PATH_DOCKER_SINK) + str(create_landing_path_data_sink(bronze_order))
-path_bronze_product = str(PATH_DOCKER_SINK) + str(create_landing_path_data_sink(bronze_product))
-path_gold_fact = str(PATH_DOCKER_SINK) + str(create_landing_path_data_sink(gold_fact_revenue))
-
-
-DataWriter(df_bronze_customer, path_bronze_customer, spark).write_df_to_delta()
-DataWriter(df_bronze_order, path_bronze_order, spark).write_df_to_delta()
-DataWriter(df_bronze_product, path_bronze_product, spark).write_df_to_delta()
-DataWriter(df_gold_fact, path_gold_fact, spark).write_df_to_delta()
+    # initialze class dynamically:
+    #bmodule = __import__(module_name)
+    #bclass_ = getattr(module, class_name)
+    #binstance = class_()
 
 
-end = time.time()
+    end = time.time()
 
-logging.basicConfig(level=logging.INFO)
